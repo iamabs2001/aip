873a002c3dd581ac1090a855547af26ca55bb0a0
sigma <sigma@localhost.localdomain>
2020-08-29 10:13:15 +0530

    NQueen update

--- practicals/nqueen.js
+++ practicals/nqueen.js
@@ -1,22 +1,19 @@
-var iterations = 0;
-
 // print the board using columns
-var print_board = function (columns) {
+function print_board(columns) {
   let n = columns.length, row = 0, col = 0;
   while (row < n) {
     while (col < n) {
       process.stdout.write(columns[row] === col ? 'Q ' : '# ');
       col++;
     }
-
     process.stdout.write('\n');
     col = 0;
     row++;
   }
 }
 
-// check if kill
-var kill = function (columns) {
+// check if killing
+function iskilling(columns) {
   let len = columns.length, last = columns[len - 1], previous = len - 2;
   while (previous >= 0) {
     if (columns[previous] === last) return true;
@@ -28,13 +25,12 @@ var kill = function (columns) {
 }
 
 // put next queen only if she does not kill other
-var place_next_queen = function (total, queens, columns) {
+function place_next_queen(total, queens, columns) {
   if (queens === 0) return columns;
   columns = columns || [];
   for (let column = 0; column < total; column++) {
     columns.push(column);
-    iterations++;
-    if (!kill(columns) && place_next_queen(total, queens - 1, columns)) return columns
+    if (!iskilling(columns) && place_next_queen(total, queens - 1, columns)) return columns
     columns.pop(column)
   }
   return null
@@ -42,4 +38,3 @@ var place_next_queen = function (total, queens, columns) {
 
 // run algorithm
 print_board(place_next_queen(4,4))
-console.log('\niterations: ', iterations)
